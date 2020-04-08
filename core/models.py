1ef3c382be4a2d81738573551dc7257ed6537be1
Agustin Gomez Mansilla <agustin.gomez.mansilla@gmail.com>
2020-04-08 16:00:53 +0200

    added country and location validated field to request. added validation method for location

--- core/models.py
+++ core/models.py
@@ -54,18 +54,19 @@ class HelpRequest(models.Model):
     added = models.DateTimeField("Agregado", auto_now_add=True, null=True, blank=True, db_index=True)
     city = models.CharField(max_length=30, blank=True, default="", editable=False)
     city_code = models.CharField(max_length=30, blank=True, default="", editable=False)
-    upvotes = models.IntegerField(default=0, blank=True)
-    downvotes = models.IntegerField(default=0, blank=True)
+    country = models.CharField(max_length=30, default="", editable=False)
+    country_code = models.CharField(max_length=30, default="", editable=False)
+    votsi = models.IntegerField(default=0, blank=True)
+    votno = models.IntegerField(default=0, blank=True)
+    loc_validated = models.BooleanField(default=False)
 
     @property
     def thumb(self):
         filepath, extension = path.splitext(self.picture.url)
         return f"{filepath}_th{extension}"
 
-    def _get_city(self):
-        geolocator = Nominatim(user_agent="ayudapy")
-        cordstr = "%s, %s" % self.location.coords[::-1]
-        location = geolocator.reverse(cordstr, language='es')
+
+    def _get_city(self, location):
         city = ''
         if location.raw.get('address'):
             if location.raw['address'].get('city'):
@@ -73,14 +74,36 @@ class HelpRequest(models.Model):
             elif location.raw['address'].get('town'):
                 city = location.raw['address']['town']
             elif location.raw['address'].get('locality'):
-                city = city = location.raw['address']['locality']
+                city = location.raw['address']['locality']
         return city
+
+
+    def _get_country(self, location):
+        country = ''
+        if location.raw.get('address'):
+            if location.raw['address'].get('country'):
+                country = location.raw['address']['country']
+        return country
+
+    def _validate_loc(self):
+        geolocator = Nominatim(user_agent="ayudapy")
+        cordstr = "%s, %s" % self.location.coords[::-1]
+        location = geolocator.reverse(cordstr, language='es')
+        if location.raw['address'].get('city'):
+            return location
+        else:
+            raise ValueError('Location invalid')
 
     def save(self):
         from unidecode import unidecode
-        city = self._get_city()
+        loc = self._validate_loc()
+        city = self._get_city(loc)
         self.city = city
         self.city_code = unidecode(city).replace(" ", "_")
+        country = self._get_country(loc)
+        self.country = country
+        self.country_code = unidecode(country).replace(" ", "_")
+        self.loc_validated = True
         return super(HelpRequest, self).save()
 
     def __str__(self):
