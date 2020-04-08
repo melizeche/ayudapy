a5c686e35f8f5d90fd1e6df32dc11b2e0fc3c2ed
Agustin Gomez Mansilla <agustin.gomez.mansilla@gmail.com>
2020-04-08 10:09:17 +0200

    renamed votsi y votno

--- core/models.py
+++ core/models.py
@@ -51,8 +51,6 @@ class HelpRequest(models.Model):
     )
     active = models.BooleanField(default=True, db_index=True)
     added = models.DateTimeField("Agregado", auto_now_add=True, null=True, blank=True, db_index=True)
-    votsi = models.IntegerField(default=0, blank=True)
-    votno = models.IntegerField(default=0, blank=True)
     upvotes = models.IntegerField(default=0, blank=True)
     downvotes = models.IntegerField(default=0, blank=True)
 
