c412c42522b58ba94a681b2240030ed6d794ba32
Miguel Báez <migueljosemaria.baez.contractor@bbva.com>
2020-04-10 01:56:34 -0400

    #6 FAQ model and admin registration added

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 from leaflet.admin import LeafletGeoAdmin
 
-from .models import HelpRequest
+from .models import HelpRequest, FrequentAskedQuestion
 
 # Register your models here.
 admin.site.register(HelpRequest, LeafletGeoAdmin)
+
+
+class FrequentAskedQuestionAdmin(admin.ModelAdmin):
+    """
+    Customize admin's FAQ change list page for easier management
+    """
+    list_display = ('question', 'order', 'active')
+    search_fields = ['question']
+    list_filter = ['active']
+
+# FAQ model registration & applied customization
+admin.site.register(FrequentAskedQuestion, FrequentAskedQuestionAdmin)
