28c82e89c0fe57a61cfbaaad854974b36cfc2808
Jonathanjojo <jonathanchristopher1199@gmail.com>
2020-04-11 19:33:45 +0700

    Change db type

--- conf/settings.py
+++ conf/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'conf.wsgi.application'
 if 'RDS_HOSTNAME' in os.environ:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.mysql',
+            'ENGINE': 'django.contrib.gis.db.backends.postgis',
             'NAME': os.environ['RDS_DB_NAME'],
             'USER': os.environ['RDS_USERNAME'],
             'PASSWORD': os.environ['RDS_PASSWORD'],
