a8f48ad91d4aacd629e11ca2a99742232afb59dd
Jean Claude Adams <tech@arrobasoft.net>
2020-04-11 14:39:35 -0400

    Update tags module with whitespace values

--- conf/urls.py
+++ conf/urls.py
@@ -29,4 +29,4 @@ urlpatterns = [
 urlpatterns += api_urls.urlpatterns
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-#urlpatterns += [path('silk/', include('silk.urls', namespace='silk'))]
+# urlpatterns += [path('silk/', include('silk.urls', namespace='silk'))]
