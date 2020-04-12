b45643b68795ec330db56a14078ba51a3a1c97b7
Jesús Alderete Insfrán <jesusalderetein@gmail.com>
2020-04-12 19:01:14 -0400

    Use conf/.env file for docker-compose

--- docker/wait-for-db.sh
+++ docker/wait-for-db.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
-# wait-for-postgres.sh
-
 set -e
 
-host="$1"
 shift
 cmd="$@"
 
-echo $host
-echo $cmd
-
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "postgres" -c '\q'; do
+until PGPASSWORD=$SECRET_KEY psql -h "db" -U "postgres" -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
