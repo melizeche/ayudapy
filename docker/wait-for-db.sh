dfbb8980967408fc52bd594be2b4a73b1ea81ecc
Jesús Alderete Insfrán <jesusalderetein@gmail.com>
2020-04-13 21:05:51 -0400

    Remove settings.py.example unused file
    Also, add ENV variables for docker-compose in .env.example

--- docker/wait-for-db.sh
+++ docker/wait-for-db.sh
@@ -4,7 +4,7 @@ set -e
 shift
 cmd="$@"
 
-until PGPASSWORD=$SECRET_KEY psql -h "db" -U "postgres" -c '\q'; do
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "db" -U $POSTGRES_USER -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
