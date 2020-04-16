e15e316e1ebb01dccc5f85182c24a4f7f11ad9ed
Marcelo Elizeche Landó <melizeche@gmail.com>
2020-04-16 05:32:39 -0400

    Fix location validation text, now the message is friendlier

--- core/forms.py
+++ core/forms.py
@@ -1,11 +1,20 @@
 from django import forms
 from leaflet.forms.widgets import LeafletWidget
+from leaflet.forms.fields import PointField
 from django.core.exceptions import NON_FIELD_ERRORS
+from django.utils.safestring import mark_safe
 
 from .models import HelpRequest
 
 
 class HelpRequestForm(forms.ModelForm):
+    location = PointField(
+        label="Ubicación",
+        error_messages={'required': mark_safe('Te olvidaste de marcar tu ubicación en el mapita\n <br>Si tenés problemas con este paso <a href="#" class="is-link modal-button" data-target="#myModal" aria-haspopup="true">mirá esta ayuda</a></p><p id="div_direccion" style="font-size: 10px; margin-bottom: 5px;"></p>')},
+        help_text=mark_safe('<p style="margin-bottom:5px;font-size:10px;">Seleccioná tu ubicación para que la gente pueda encontrarte, si no querés marcar tu casa una buena opción puede ser la comisaría más cercana o algún otro sitio público cercano.\
+            <br>Si tenés problemas con este paso <a href="#" class="is-link modal-button" data-target="#myModal" aria-haspopup="true">mirá esta ayuda</a></p><p id="div_direccion" style="font-size: 10px; margin-bottom: 5px;"></p>'),
+        )
+
     class Meta:
         model = HelpRequest
         fields = (
@@ -18,7 +27,6 @@ class HelpRequestForm(forms.ModelForm):
             "picture",
         )
         widgets = {
-            "location": LeafletWidget(),
             "title": forms.TextInput(
                 attrs={
                     "class": "input",
