b3124304e87f9fd2ad0b7b7e6493036877433634
Marcelo Elizeche Landó <melizeche@gmail.com>
2020-04-17 03:11:20 -0400

    use host for master

--- conf/settings.py
+++ conf/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['ayudapy.org']
 
 DEBUG_PROPAGATE_EXCEPTIONS = True
 
