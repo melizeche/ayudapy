fee0083bba68ac8a7e8e8af23774b00cc378d552
Grosip <grosip@gmail.com>
2020-04-19 23:02:34 -0400

    Agregamos icono verde en donaciones - 3

--- static/scripts/list-donation.js
+++ static/scripts/list-donation.js
@@ -112,7 +112,7 @@
 
       var greenIcon = new L.Icon({
         iconUrl:
-          'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+          '/static/icons/marker-icon-2x-green.png',
         iconSize: [25, 41],
         iconAnchor: [12, 41],
         popupAnchor: [1, -34],
