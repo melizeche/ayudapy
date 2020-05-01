10e37c9b6c4381edce071522bc19f7f3e01f3e1a
Martin Abente Lahaye <martin.abente.lahaye@gmail.com>
2020-05-01 22:08:02 +0000

    I18N Support - part 2
    A few other views, models and forms, but, there are Still
    many more to translate.

--- core/forms.py
+++ core/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from leaflet.forms.fields import PointField
 from django.core.exceptions import NON_FIELD_ERRORS
 from django.utils.safestring import mark_safe
+from django.utils.translation import gettext as _
 
 from .models import HelpRequest
 
@@ -9,9 +10,17 @@ from .models import HelpRequest
 class HelpRequestForm(forms.ModelForm):
     location = PointField(
         label="Ubicación",
-        error_messages={'required': mark_safe('Te olvidaste de marcar tu ubicación en el mapita\n <br>Si tenés problemas con este paso <a href="#" class="is-link modal-button" data-target="#myModal" aria-haspopup="true">mirá esta ayuda</a></p><p id="div_direccion" style="font-size: 10px; margin-bottom: 5px;"></p>')},
-        help_text=mark_safe('<p style="margin-bottom:5px;font-size:10px;">Seleccioná tu ubicación para que la gente pueda encontrarte, si no querés marcar tu casa una buena opción puede ser la comisaría más cercana o algún otro sitio público cercano.\
-            <br>Si tenés problemas con este paso <a href="#" class="is-link modal-button" data-target="#myModal" aria-haspopup="true">mirá esta ayuda</a></p><p id="div_direccion" style="font-size: 10px; margin-bottom: 5px;"></p>'),
+        # XXX Move all HTML to the corresponding templates
+        error_messages={'required': mark_safe('{}\n<br>{} <a href="#" class="is-link modal-button" data-target="#myModal" aria-haspopup="true">{}</a></p><p id="div_direccion" style="font-size: 10px; margin-bottom: 5px;"></p>'.format(
+            _("You forgot to mark your location on the map"),
+            _("If you have problems with this step"),
+            _("Check out this help"),
+            ))},
+        help_text=mark_safe('<p style="margin-bottom:5px;font-size:10px;">{}.<br>{} <a href="#" class="is-link modal-button" data-target="#myModal" aria-haspopup="true">{}</a></p><p id="div_direccion" style="font-size: 10px; margin-bottom: 5px;"></p>'.format(
+            _("Select your location so that people can find you, if you do not want to mark your home a good option may be the nearest police station or some other nearby public place."),
+            _("If you have problems with this step"),
+            _("Check out this help"),
+            )),
         )
 
     class Meta:
@@ -30,15 +39,14 @@ class HelpRequestForm(forms.ModelForm):
             "title": forms.TextInput(
                 attrs={
                     "class": "input",
-                    "placeholder": "Ejemplo: Necesito de manera urgente víveres para mi familia",
+                    "placeholder": _("Example: I urgently need food for my family"),
                 }
             ),
             "message": forms.Textarea(
                 attrs={
                     "class": "textarea",
                     "rows": 4,
-                    "placeholder": "Ejemplo: Por la situación actual estoy necesitando tapabocas y productos de limpieza, \
-                        cualquier ayuda aunque sea mínima ya me va a ayudar.\nMuchas Gracias!",
+                    "placeholder": _("Example: Due to the current situation I am in need of masks and cleaning products, any help, even a minimal one, will help me. Thank you so much!"),
                 }
             ),
             "name": forms.TextInput(attrs={"class": "input"}),
@@ -48,6 +56,6 @@ class HelpRequestForm(forms.ModelForm):
         }
         error_messages = {
             NON_FIELD_ERRORS: {
-                'unique_together': "Registro ya ingresado, no puede duplicar mismo pedido.",
+                'unique_together': _("Registration already entered, cannot duplicate the same request."),
             }
         }
